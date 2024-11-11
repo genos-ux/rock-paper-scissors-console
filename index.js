62a75535cc49b186730af55a63680d651fadc147
genos-ux <gabriel.kyeremateng.gk@gmail.com>
2024-11-11 10:01:19 +0000

    Created a playground function for the user and computer

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getRandomChoice(max)
 {
     return Math.floor(Math.random() * max);
@@ -15,5 +18,62 @@ const getComputerChoice = () =>
 const getHumanChoice = () =>
 {
     let choice = prompt("Choose Rock,Paper or Scissors");
-    return choice;
+    return choice.toLowerCase();
+}
+
+function playGround(humanChoice,computerChoice)
+{
+    let msg = "";
+
+    if(humanChoice == "rock")
+    {
+        switch(computerChoice)
+        {
+            case "rock":
+                msg = "You tie!";
+                break;
+            case "paper":
+                msg = "You lose! Paper beats Rock.";
+                break;
+            case "scissors":
+                msg = "You win! Rock beats scissors";
+
+        }
+    }
+
+    else if(humanChoice == "paper")
+    {
+        switch(computerChoice)
+        {
+            case "paper":
+                msg = "You tie!";
+                break;
+            case "scissors":
+                msg = "You lose! Scissors beats Paper.";
+                break;
+            case "rock":
+                msg = "You win! Paper beats Rock";
+                break;
+
+        }
+    }
+
+    else if(humanChoice == "scissors")
+    {
+        switch(computerChoice)
+        {
+            case "scissors":
+                msg = "You tie!";
+                break;
+            case "rock":
+                msg = "You lose! Rock beats Scissors.";
+                break;
+            case "paper":
+                msg = "You win! Paper beats Scissors";
+                break;
+
+        }
+    }
+
+
 }
